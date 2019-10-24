5739b43c1f2c03233f73c80548eb8cc1202a45c8
spongybobb <thzc0621@outlook.com>
2019-10-24 10:47:22 +0100

    v1.1 add class Rect

--- src/Drawing.java
+++ src/Drawing.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowEvent;
+import java.awt.event.WindowAdapter;
 
 
 
@@ -8,17 +9,26 @@ public class Drawing extends Canvas {
 
     private Frame f;
     private Circle circle;
+    private Rect rectangle;
 
     public Drawing(){
         Point p=new Point(200,200);
-        Color c=new Color(0x992266);//  The  RGB  number  comprises  three  bytes:
+        Color c=new Color(0x992566);//  The  RGB  number  comprises  three  bytes:
+        Point p1=new Point(230,230);
+        Color c1=new Color(0x998300);//  The  RGB  number  comprises  three  bytes:
+
         circle = new Circle(p,c,200);
+        rectangle = new Rect(p1,c1,100,50);
         f  =  new  Frame("My  window");    //  Instantiates  the  Frame
         f.add(this);   // Adds the Canvas to the Frame
         f.setLayout(null); //  Stops  the  frame  from  trying  to  layout  contents
         f.setSize(400,  400);  //  Sets  the  dimensions  of  the  frame
         f.setVisible(true);
-        //f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//  Closes  the  program  if close window clicked
+        f.addWindowListener(new WindowAdapter() {	// Closes the frame if close window clicked
+            public void windowClosing(WindowEvent e) {
+                f.dispose();
+            }
+        });
         setBackground(Color.WHITE);  //  Sets  the  Canvas  background
         setSize(400,  400);    //  Sets  the  Canvas  size  to  be  the  same  as  the  frame
 
@@ -29,6 +39,7 @@ public class Drawing extends Canvas {
 
     public void paint(Graphics g) {
     circle.draw(g);
+    rectangle.draw(g);
 
 
 
--- src/Rect.java
+++ src/Rect.java
@@ -1,3 +1,23 @@
+import java.awt.*;
+
 public class Rect {
+
+    private int width; // Fields
+    private int height;
+    private Point pos;
+    private Color col;
+    public Rect(Point initPos, Color col, int width, int height){// The constructor
+
+        this.width =width;// Initialize fields
+        this.height =height;
+        pos =initPos;
+        this.col =col;
+
+    }
+
+    public void draw(Graphics g) {// A method that draws the object in g
+        g.setColor(col);
+        g.fillRect(pos.x, pos.y, width, height);
+    }
 
 }
